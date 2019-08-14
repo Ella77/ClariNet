19530a0b51beb85bf302ae5ad2c92fc410dd6b0d
Ellie Kang <ksjbang@gmail.com>
2019-08-14 17:34:46 +0900

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -13,9 +13,10 @@ def build_from_path(in_dir, out_dir, num_workers=1):
     index = 1
     with open(os.path.join(in_dir, 'metadata.csv'), encoding='utf-8') as f:
         for line in f:
-            parts = line.strip().split('|')
-            wav_path = os.path.join(in_dir, 'wavs', '%s.wav' % parts[0])
-            text = parts[2]
+            parts = line.strip().split(',')
+            #wav_path = os.path.join(in_dir, 'wavs', '%s.wav' % parts[0])
+            wav_path = os.path.join('/home/soojin/waveglow/',parts[0])
+            text = parts[1]
             futures.append(executor.submit(
                 partial(_process_utterance, out_dir, index, wav_path, text)))
             index += 1
